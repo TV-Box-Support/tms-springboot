e3cd6c5e8db6cc3aa890d7d2a6571b576fcde234
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2023-05-08 10:51:57 +0700

    commit

--- src/main/java/com/vnptt/tms/TmsApplication.java
+++ src/main/java/com/vnptt/tms/TmsApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class TmsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TmsApplication.class, args);
 	}
