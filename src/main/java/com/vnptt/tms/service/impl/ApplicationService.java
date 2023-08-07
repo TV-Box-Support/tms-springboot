998713fe6eb5ecd6ae00524c44fd7d3672d5684a
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2023-08-07 13:50:41 +0700

    master: Update funtion show list policy

--- src/main/java/com/vnptt/tms/service/impl/ApplicationService.java
+++ src/main/java/com/vnptt/tms/service/impl/ApplicationService.java
@@ -230,7 +230,7 @@ public class ApplicationService implements IApplicationService {
     @Override
     public List<DoubleBarChart> getBarChartApplicationDowload() {
         List<DoubleBarChart> result = new ArrayList<>();
-        List<ApplicationEntity> list = applicationRepository.findTop4DistinctApplicationEntitiesByIssystemOrderByDeviceApplicationEntitiesDesc(false);
+        List<ApplicationEntity> list = applicationRepository.findTop4ApplicationEntitiesByIssystemOrderByDeviceApplicationEntitiesDesc(false);
 
         for (ApplicationEntity entity : list) {
             LocalDateTime time = LocalDateTime.now().plusMinutes(-3);
