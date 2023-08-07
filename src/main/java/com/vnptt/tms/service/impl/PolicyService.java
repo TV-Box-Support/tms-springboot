c4b3792eb9a92a852f2c50b8821eedb85f5268cd
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2023-08-07 11:28:12 +0700

    master: Update funtion show list policy

--- src/main/java/com/vnptt/tms/service/impl/PolicyService.java
+++ src/main/java/com/vnptt/tms/service/impl/PolicyService.java
@@ -71,7 +71,7 @@ public class PolicyService implements IPolicyService {
                 policyEntity.setCommandEntity(commandEntity);
             }
         } else {
-            if(policyDTO.getAction() == 3){
+            if (policyDTO.getAction() == 3) {
                 throw new RuntimeException("Action Run Command cann't null command");
             }
         }
@@ -215,11 +215,15 @@ public class PolicyService implements IPolicyService {
                     }
                     break;
                 case 3:
-                    iteam.setStatus(4);
+                    if (iteam.getStatus() == 1) {
+                        iteam.setStatus(4);
+                    }
+
 
             }
 
         }
+
         policyEntity = policyRepository.save(policyEntity);
         return policyConverter.toDTO(policyEntity);
     }
@@ -276,7 +280,7 @@ public class PolicyService implements IPolicyService {
             result.add(new PieChart(pause, "Pause"));
             result.add(new PieChart(stop, "Stop"));
             result.add(new PieChart(notRun, "Not Run"));
-        } else if(Objects.equals(type, "action")){
+        } else if (Objects.equals(type, "action")) {
             Long install = policyRepository.countAllByAction(1);
             Long uninstall = policyRepository.countAllByAction(2);
             Long runCommand = policyRepository.countAllByAction(3);
